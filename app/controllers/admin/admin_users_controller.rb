94bdb01cc2b1dee21be2e5408eac1957ed3f65b2
Jason <jasonzahne@gmail.com>
2017-01-06 10:49:11 -0700

    Fixed bug in admin dashboard

--- app/controllers/admin/admin_users_controller.rb
+++ app/controllers/admin/admin_users_controller.rb
@@ -1,5 +1,5 @@
 module Admin
-  class AdminUsersController < Admin::ApplicationController
+  class EmployeesController < Admin::ApplicationController
     # To customize the behavior of this controller,
     # simply overwrite any of the RESTful actions. For example:
     #
